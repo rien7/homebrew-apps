c660ea8db88b055b74e836ec77dd4125f2e4904d
RieN 7z <zrien7@gmail.com>
2025-03-13 06:46:26 +0800

    fix aeroindicator 1.0.3

--- Casks/aeroindicator.rb
+++ Casks/aeroindicator.rb
@@ -3,7 +3,7 @@
 
 cask "aeroindicator" do
   version "1.0.3"
-  sha256 "2ad95b738449f836b537c0354a4e49ee3b80b2dfae316d85eae6f0189d8eeb2b"
+  sha256 "81243af0d75264d3f8f33808d7f33f144d13588c6d2cd01f78b86b0efc2332bb"
 
   url "https://github.com/rien7/AeroIndicator/releases/download/v#{version}/AeroIndicator.zip"
   name "AeroIndicator"
