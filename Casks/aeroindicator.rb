ad4d9f0cb5a3b495e8d1296da68b56662a156334
RieN 7z <zrien7@gmail.com>
2025-03-17 13:54:16 +0800

    add aeroindicator 1.0.6

--- Casks/aeroindicator.rb
+++ Casks/aeroindicator.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "aeroindicator" do
-  version "1.0.5"
-  sha256 "78f77c42b0c519aebf14d62a8fe268a09f552e4d8530a1ad7975fc7ef1b5f4f1"
+  version "1.0.6"
+  sha256 "6ffb56b80d56298c041ce3b8959af560f769adfa60d5f1739b62626e45976370"
 
   url "https://github.com/rien7/AeroIndicator/releases/download/v#{version}/AeroIndicator.zip"
   name "AeroIndicator"
